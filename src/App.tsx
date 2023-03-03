01e5b0c529f5e78546c7a410d41d6661d7c5abd6
JiseokLee <jiseoklee0502@gmail.com>
2023-03-03 15:14:23 +0900

    react-sliding-pane 라이브러리 사용, App 컴포넌트에서 props 통해 작동 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
+import { useState } from "react";
+
 import Workspace from "./components/Workspace/Workspace";
+import Chatting from "./components/Chatting/Chatting";
 
 import classes from "./App.module.css";
 
 function App() {
+  const [isChattingOpen, setIsChattingOpen] = useState(false);
+
+  const openChattingHandler = () => setIsChattingOpen(true);
+
+  const closeChattingHandler = () => setIsChattingOpen(false);
+
   return (
     <div>
+      <button onClick={openChattingHandler}>
+        채팅 슬라이딩 패널 테스트 버튼
+      </button>
       <Workspace />
+      <Chatting
+        isChattingOpen={isChattingOpen}
+        onCloseChatting={closeChattingHandler}
+      />
     </div>
   );
 }
--- src/components/Chatting/Chatting.tsx
+++ src/components/Chatting/Chatting.tsx
@@ -1,3 +1,24 @@
-function Chatting() {}
+import { ReactSlidingPane } from "react-sliding-pane";
+import "react-sliding-pane/dist/react-sliding-pane.css";
+
+type ChattingProps = {
+  isChattingOpen: boolean;
+  onCloseChatting: () => void;
+};
+
+function Chatting(props: ChattingProps) {
+  return (
+    <div>
+      <ReactSlidingPane
+        isOpen={props.isChattingOpen}
+        width="25%"
+        onRequestClose={props.onCloseChatting}
+      >
+        <h1>Sliding Pane Test</h1>
+        <p>채팅 UI 구현하기</p>
+      </ReactSlidingPane>
+    </div>
+  );
+}
 
 export default Chatting;
