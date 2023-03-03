83e76015736fca83c38d0a7c407a6ddedc8b85e0
Jisu Han <haneh1501@naver.com>
2023-03-03 19:54:53 +0900

    일정추가 및 요일 선택 가능

--- src/components/Workspace/Schedule/MyCalendar.tsx
+++ src/components/Workspace/Schedule/MyCalendar.tsx
@@ -1,19 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-import interactionPlugin from "@fullcalendar/interaction";
-import timeGridPlugin from '@fullcalendar/timegrid'
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import {
+  DateSelectArg,
+  EventApi,
+  EventClickArg,
+  EventContentArg,
+} from "@fullcalendar/core";
+
+let id = 0;
 
 function MyCalendar() {
+  const [events, setEvents] = useState<Array<EventApi>>([])
+  const [initialEvents, setInitialEvents] = useState([
+    {
+      id: String(10001),
+      title: "일정1",
+      start: new Date().toISOString().split("T")[0],
+    },
+    {
+      id: String(10002),
+      title: "일정2",
+      start: new Date().toISOString().split("T")[0] + "T14:05:00",
+    },
+  ]);
+useEffect(()=>{console.log("event", events)}, [events])
+  const handleEvents=(events:EventApi[]) => {
+    setEvents(events)
+  }
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    return (
+      <>
+        <b>Time Text: {eventInfo.timeText}</b>
+        <b>Title: {eventInfo.event.title}</b>
+      </>
+    );
+  };
+
+  const handleEventClick = (clickInfo: EventClickArg) => {
+    alert(`일정 이름: ${clickInfo.event.title}`);
+    console.log(clickInfo.event.id);
+  };
+
+  const handleDateSelect = (selectInfo: DateSelectArg) => {
+    let title = prompt("event");
+    let calendarApi = selectInfo.view.calendar;
+    calendarApi.unselect()
+    if(title) {
+      calendarApi.addEvent({
+        id:String(id++),
+        title,
+        start:selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay:selectInfo.allDay
+      })
+    }
+  };
   return (
     <>
-    <FullCalendar
-      plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-      initialView="dayGridMonth"
-    />
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        headerToolbar={{
+          left: "dayGridMonth, timeGridWeek, timeGridDay",
+          center: "title",
+          right: "prev, next today",
+        }}
+        dateClick={(e: DateClickArg) => {
+          console.log("dateclick", e);
+        }}
+        select={handleDateSelect}
+        eventClick={handleEventClick}
+        eventContent={renderEventContent}
+        locale="en"
+        initialView="dayGridMonth"
+        selectable={true}
+        eventsSet={handleEvents}
+        editable={true}
+        initialEvents={initialEvents}
+        dayMaxEvents={false}
+      />
     </>
   );
-  
 }
 
 export default MyCalendar;
