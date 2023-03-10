d9d6d3a5293be88ba412d8bd15dd9d1759d2b474
JiseokLee <jiseoklee0502@gmail.com>
2023-03-10 15:30:41 +0900

    상단 바 메뉴 요소들에 Popover UI 적용

--- src/components/TopMenuBar/Alarms.tsx
+++ src/components/TopMenuBar/Alarms.tsx
@@ -1,10 +1,21 @@
+import { OverlayTrigger, Popover } from "react-bootstrap";
 import { BsFillBellFill } from "react-icons/bs";
 
 function Alarms() {
   return (
-    <h3>
-      <BsFillBellFill /> 알림
-    </h3>
+    <OverlayTrigger
+      trigger="click"
+      placement="bottom"
+      overlay={
+        <Popover>
+          <Popover.Body>Alarms Test Popover</Popover.Body>
+        </Popover>
+      }
+    >
+      <h3>
+        <BsFillBellFill /> 알림
+      </h3>
+    </OverlayTrigger>
   );
 }
 
--- src/components/TopMenuBar/Search.tsx
+++ src/components/TopMenuBar/Search.tsx
@@ -1,10 +1,21 @@
+import { OverlayTrigger, Popover } from "react-bootstrap";
 import { AiOutlineSearch } from "react-icons/ai";
 
 function Search() {
   return (
-    <h3>
-      <AiOutlineSearch /> 검색
-    </h3>
+    <OverlayTrigger
+      trigger="click"
+      placement="bottom"
+      overlay={
+        <Popover>
+          <Popover.Body>Search Test Popover</Popover.Body>
+        </Popover>
+      }
+    >
+      <h3>
+        <AiOutlineSearch /> 검색
+      </h3>
+    </OverlayTrigger>
   );
 }
 
