d0b443b5de909de272284091e4fe9ba9b6da9ea1
JiseokLee <jiseoklee0502@gmail.com>
2023-03-13 14:40:09 +0900

    폴더 구조 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Workspace from "./components/Workspace/Workspace";
 
 import classes from "./App.module.css";
-import MyCalendar from "./components/Workspace/Schedule/MyCalendar";
+import MyCalendar from "./components/Schedule/MyCalendar";
 
 function App() {
   return (
