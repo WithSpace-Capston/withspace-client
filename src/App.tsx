69ffd98a68de27cec382f540df8380deefa13825
JiseokLee <jiseoklee0502@gmail.com>
2023-03-25 15:27:46 +0900

    라우팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 import SideMenuBar from "./components/SideMenuBar/SideMenuBar";
 import TopMenuBar from "./components/TopMenuBar/TopMenuBar";
 import Workspace from "./components/Workspace/Workspace";
-import Chatting from "./components/Chatting/Chatting";
 import MyCalendar from "./components/Schedule/MyCalendar";
+import RootNavigator from "./RootNavigator";
 import { useUIState } from "./contexts/UIContext";
 
 import "./App.css";
@@ -11,17 +13,21 @@ function App() {
   const uiState = useUIState();
 
   return (
-    <div className="app">
-      <div className="side">
-        <SideMenuBar />
+    <BrowserRouter>
+      <div className="app">
+        <div className="side">
+          <SideMenuBar />
+        </div>
+        <div className="main">
+          <TopMenuBar />
+          <Routes>
+            <Route path="/" element={<RootNavigator />} />
+            <Route path="/:userId/space" element={<Workspace />} />
+            <Route path="/:userId/calendar" element={<MyCalendar />} />
+          </Routes>
+        </div>
       </div>
-      <div className="main">
-        <TopMenuBar />
-        {uiState.isSpaceWorkspace && <Workspace />}
-        {!uiState.isSpaceWorkspace && <MyCalendar />}
-        <Chatting />
-      </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/RootNavigator.tsx
@@ -0,0 +1,7 @@
+import { Navigate } from "react-router-dom";
+
+function RootNavigator() {
+  return <Navigate to="/2/space"></Navigate>;
+}
+
+export default RootNavigator;
