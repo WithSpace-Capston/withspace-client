207e6ad69d2ff1244ff118c8211e561cea0d5b6d
han-jisu0525 <75937116+han-jisu0525@users.noreply.github.com>
2023-04-17 02:24:14 +0900

    Update index.tsx
    recoil, globalstyle 태그 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 
 import "./index.css";
 import App from "./App";
 import { UIContextProvider } from "./contexts/UIContext";
+import { TeamContextProvider } from "./contexts/TeamContext";
+import { RecoilRoot } from "recoil";
+import { GlobalStyle } from "./components/Schedule/styles/global-style";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <UIContextProvider>
-    <App />
+    <TeamContextProvider>
+      <RecoilRoot>
+        <GlobalStyle />
+        <App />
+      </RecoilRoot>
+    </TeamContextProvider>
   </UIContextProvider>
 );
