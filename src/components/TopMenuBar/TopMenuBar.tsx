ddef95720921543bbbf6374a36a91f4dc28765f9
JiseokLee <jiseoklee0502@gmail.com>
2023-04-30 18:34:33 +0900

    TopMenuBar 스타일 리팩토링

--- src/components/TopMenuBar/TopMenuBar.tsx
+++ src/components/TopMenuBar/TopMenuBar.tsx
@@ -1,20 +1,35 @@
+import styled from "styled-components";
+
 import Members from "./Members/Members";
 import Alarms from "./Alarms";
 import Search from "./Search";
 import Update from "./Update";
 import ETCMenu from "./ETCMenu";
-import "./TopMenuBar.css";
 
 function TopMenuBar() {
   return (
-    <div className="menu">
+    <Menu>
       <Members />
       <Alarms />
       <Search />
       <Update />
       <ETCMenu />
-    </div>
+    </Menu>
   );
 }
 
 export default TopMenuBar;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  margin: 10px 0;
+  height: 35px;
+
+  h3 {
+    font-weight: normal;
+    font-size: 20px;
+    margin: 0 15px;
+  }
+`;
